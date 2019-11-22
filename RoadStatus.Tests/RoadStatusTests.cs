11a34dc115c8ad9556cac53c3c687559af5039ff
pcmalik <praveen_malik@hotmail.com>
2019-11-22 08:53:52 +0000

    Added appsettings and empty test scenarios cases

--- RoadStatus.Tests/RoadStatusTests.cs
+++ RoadStatus.Tests/RoadStatusTests.cs
@@ -1,13 +1,44 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace RoadStatus.Tests
 {
     [TestClass]
     public class RoadStatusTests
     {
+        #region Valid Road Scenarios
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetRoadStatus_When_ValidRoadId_Then_DisplayName_ShouldNotBeNullOrEmpty()
         {
+            throw new NotImplementedException();
         }
+
+        [TestMethod]
+        public void TestGetRoadStatus_When_ValidRoadId_Then_RoadStatus_ShouldNotBeNullOrEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TestGetRoadStatus_When_ValidRoadId_Then_StatusSeverityDescription_ShouldNotBeNullOrEmpty()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Invalid Road Scenarios
+        [TestMethod]
+        public void TestGetRoadStatus_When_InValidRoadId_Then_Return_InformativeError()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TestGetRoadStatus_When_InValidRoadId_Then_Return_NonZeroSystemErrorCode()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
     }
 }
