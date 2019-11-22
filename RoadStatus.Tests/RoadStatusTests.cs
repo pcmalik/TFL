587e83cf1c8ff946df0ce87c743f39d1a97861f6
pcmalik <praveen_malik@hotmail.com>
2019-11-22 08:34:22 +0000

    Initial project strucuture

---
+++ RoadStatus.Tests/RoadStatusTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RoadStatus.Tests
+{
+    [TestClass]
+    public class RoadStatusTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
