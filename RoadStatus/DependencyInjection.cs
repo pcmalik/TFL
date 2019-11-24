049889d94132aff2a23de53c51b8a93010eeebc0
pcmalik <praveen_malik@hotmail.com>
2019-11-24 10:57:30 +0000

    Refactored dependencies

---
+++ RoadStatus/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RoadStatus.Models;
+using RoadStatus.Repositories;
+using RoadStatus.Repositories.Interfaces;
+using System;
+using System.IO;
+
+namespace RoadStatus
+{
+    public static class DependencyInjection
+    {
+        public static IServiceProvider ConfigureServices()
+        {
+            var serviceCollection = new ServiceCollection();
+            // build configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+            serviceCollection.AddOptions();
+            serviceCollection.Configure<AppSettings>(configuration.GetSection("Configuration"));
+
+            // add services
+            serviceCollection.AddScoped<IRoadStatusRepository, RoadStatusRepository>();
+            serviceCollection.AddScoped<IHttpServiceRepository, HttpServiceRepository>();
+            serviceCollection.AddScoped<IStatusTracker, StatusTracker>();
+
+            // add application
+            serviceCollection.AddSingleton<TFLApplication>();
+
+            return serviceCollection.BuildServiceProvider();
+        }
+    }
+}
--- RoadStatus/Program.cs
+++ RoadStatus/Program.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using RoadStatus.Models;
-using RoadStatus.Repositories;
-using RoadStatus.Repositories.Interfaces;
-using System;
-using System.IO;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace RoadStatus
 {
@@ -12,35 +6,11 @@ namespace RoadStatus
     {
         public static void Main(string[] args)
         {
-            // create service collection
-            var serviceCollection = new ServiceCollection();
-            ConfigureServices(serviceCollection);
-
-            // create service provider
-            var serviceProvider = serviceCollection.BuildServiceProvider();
+            // Configure dependencies
+            var serviceProvider = DependencyInjection.ConfigureServices();
 
             // run app
             serviceProvider.GetService<TFLApplication>().Run();
         }
-
-        private static void ConfigureServices(IServiceCollection serviceCollection)
-        {
-            // build configuration
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", false)
-                .Build();
-            serviceCollection.AddOptions();
-            serviceCollection.Configure<AppSettings>(configuration.GetSection("Configuration"));
-
-            // add services
-            serviceCollection.AddScoped<IRoadStatusRepository, RoadStatusRepository>();
-            serviceCollection.AddScoped<IHttpServiceRepository, HttpServiceRepository>();
-            serviceCollection.AddScoped<IStatusTracker, StatusTracker>();
-
-            // add application
-            serviceCollection.AddScoped<TFLApplication>();
-        }
-
     }
 }
